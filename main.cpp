7b3da713f65a5c176df1feb561cce05690ba0871
Elia Maillard-Salin <147042560+EliaMaillard-Salin@users.noreply.github.com>
2025-11-04 08:56:24 +0100

    Update main.cpp
    Question 1.2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
+#include <Windows.h>
+
+// Clear : [2J
+// SetCursor 0,0 : [H
+// Cursor Invisible : [?25l
+// Cursor Visible : [?25h
+
+// ANSI Command : https://gist.github.com/fnky/458719343aabd01cfb17a3a4f7296797
+
+
 int main(int argc, char** argv)
 {
     std::cout << "Hello World!\n";
+    
+    std::cout << "\033[H";
+    std::cout << "\033[2J";
+    std::cout << "\033[?25h";
+
+    while(1){}
+
     return 0;
 }
